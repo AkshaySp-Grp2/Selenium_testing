eb212aa8d9f58acc2bf43833b429ee8738b3c590
Akshay <82815124+AkshaySp-Grp2@users.noreply.github.com>
2022-06-07 18:09:24 +0530

    Oops applied

--- src_py/main.py
+++ src_py/main.py
@@ -5,55 +5,72 @@ import pandas as pd
 import time
 import os
 
-dt = pd.read_csv(os.getcwd() +"\\"+ 'data.csv')
+class Insert():
 
 
-url = "http://localhost:1085/"
 
-driver = webdriver.Chrome("chromedriver.exe")
-driver.get(url)
-
-dir(driver)
-
-email = driver.find_element(by=  By.NAME, value='name')
-email.send_keys(dt['name'][0])
-time.sleep(2)
-email = driver.find_element(by = By.NAME, value='email')
-email.send_keys(dt['email'][0])
-time.sleep(2)
+    def __init__(self,driver):
+        self.driver = driver
+        # self.driver = webdriver.Chrome("chromedriver.exe")
+        self.url = "http://localhost:1085/"
+        self.driver.get(self.url)
 
 
-email = driver.find_element(by = By.NAME, value='next').click()
-time.sleep(2)
-email = driver.find_element(by = By.NAME, value='username')
-email.send_keys(dt['username'][0])
-time.sleep(2)
-email = driver.find_element(by = By.NAME, value='password')
-email.send_keys(dt['password'][0])
-time.sleep(2)
+    def get_data(self,data):
+        self.data = data
+        self.dt = pd.read_csv(os.getcwd() +"\\"+ self.data)
 
-email = driver.find_element(by = By.NAME, value='next').click()
-time.sleep(2)
+# dt = pd.read_csv(os.getcwd() +"\\"+ 'data.csv')
 
-email = driver.find_element(by = By.NAME, value='number')
-dt['number'] = dt['number'].to_string()
-email.send_keys(dt['number'][0])
-time.sleep(2)
-email = driver.find_element(by = By.NAME, value='DOB')
-email.send_keys(dt['DOB'][0])
-time.sleep(2)
-email = driver.find_element(by = By.NAME, value='next').click()
-time.sleep(2)
 
-if dt['gender'][0] == "Male":
-    email = driver.find_element_by_xpath("//html//body//form//div[4]//input[1]").click()
-elif dt['gender'][0] == "Female":
-    email = driver.find_element_by_xpath("//html//body//form//div[4]//input[2]").click()
-else:
-    email = driver.find_element_by_xpath("//html//body//form//div[4]//input[3]").click()
-time.sleep(2)
+# url = "http://localhost:1085/"
 
-driver.close()
+# driver = webdriver.Chrome("chromedriver.exe")
+# driver.get(url)
+
+    def insert(self):
+        
+                
+
+        email = self.driver.find_element(by=  By.NAME, value='name')
+        email.send_keys(self.dt['name'][0])
+        time.sleep(2)
+        email = self.driver.find_element(by = By.NAME, value='email')
+        email.send_keys(self.dt['email'][0])
+        time.sleep(2)
+
+
+        email = self.driver.find_element(by = By.NAME, value='next').click()
+        time.sleep(2)
+        email = self.driver.find_element(by = By.NAME, value='username')
+        email.send_keys(self.dt['username'][0])
+        time.sleep(2)
+        email = self.driver.find_element(by = By.NAME, value='password')
+        email.send_keys(self.dt['password'][0])
+        time.sleep(2)
+
+        email = self.driver.find_element(by = By.NAME, value='next').click()
+        time.sleep(2)
+
+        email = self.driver.find_element(by = By.NAME, value='number')
+        self.dt['number'] = self.dt['number'].to_string()
+        email.send_keys(self.dt['number'][0])
+        time.sleep(2)
+        email = self.driver.find_element(by = By.NAME, value='DOB')
+        email.send_keys(self.dt['DOB'][0])
+        time.sleep(2)
+        email = self.driver.find_element(by = By.NAME, value='next').click()
+        time.sleep(2)
+
+        if self.dt['gender'][0] == "Male":
+            email = self.driver.find_element_by_xpath("//html//body//form//div[4]//input[1]").click()
+        elif self.dt['gender'][0] == "Female":
+            email = self.driver.find_element_by_xpath("//html//body//form//div[4]//input[2]").click()
+        else:
+            email = self.driver.find_element_by_xpath("//html//body//form//div[4]//input[3]").click()
+        time.sleep(2)
+
+        self.driver.close()
 
 # email = driver.find_element_by_name('address')
 # email.send_keys(dt['address'][0])
@@ -65,5 +82,11 @@ driver.close()
 # email.send_keys(dt['lname'][0])
 # time.sleep(2)
 
+if __name__ =="__main__":
 
-driver.quit()
+
+    driver = webdriver.Chrome("chromedriver.exe")
+    pp = Insert(driver)
+    data = 'data.csv'
+    pp.get_data(data=data)
+    pp.insert()
